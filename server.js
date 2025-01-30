1bc7f7965f937593241961ce66763de5b47ed272
sherlocKPath <143311874+sherlocKPath@users.noreply.github.com>
2025-01-30 15:06:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ function generateToken() {
 
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb+srv://petchza10222:1652038ZXCV@peth.3o5dx.mongodb.net//Booker")
+  .connect("mongodb+srv://petchza10222:1652038ZXCV@peth.3o5dx.mongodb.net/Booker")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
